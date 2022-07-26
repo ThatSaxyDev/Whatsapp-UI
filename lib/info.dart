5cbbaf687fb8e54bcd900f9f73a4ffebff54c4f7
Iteoluwakiisi David Dedeke <thatsaxydev@gmail.com>
2022-07-26 08:16:45 +0100

    Whatsapp UI

---
+++ lib/info.dart
@@ -0,0 +1,137 @@
+const info = [
+  {
+    'name': 'Elon Musk',
+    'message': 'Yo, Kiish!, wa gbemi trabaye',
+    'time': '3:53 pm',
+    'profilePic':
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/220px-Elon_Musk_Royal_Society_%28crop2%29.jpg',
+  },
+  {
+    'name': 'Olamide',
+    'message': 'You no go like show studio',
+    'time': '2:25 pm',
+    'profilePic':
+        'https://nation-media-assets.storage.googleapis.com/wp-content/uploads/2022/02/02141928/OLA.jpg',
+  },
+  {
+    'name': 'Starboy',
+    'message': 'You suppose dey that Balon video sha',
+    'time': '1:03 pm',
+    'profilePic':
+        'https://www.essence.com/wp-content/uploads/2021/10/WizKid-1200x900.jpg?width=1200',
+  },
+  {
+    'name': 'DonJazzy',
+    'message': 'Shey Rihanna don text you?',
+    'time': '12:06 pm',
+    'profilePic':
+        'https://nation-media-assets.storage.googleapis.com/wp-content/uploads/2021/11/02165626/jazzy.jpg',
+  },
+  {
+    'name': 'RiRi',
+    'message': 'Shit went rocky ASAP Kiishi',
+    'time': '10:00 am',
+    'profilePic':
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rocky-and-rihanna-celebrate-fenty-beauty-fenty-skin-at-goya-news-photo-1649776150.jpg',
+  },
+  {
+    'name': 'ASAP Rocky',
+    'message': 'They are lying bro',
+    'time': '9:53 am',
+    'profilePic':
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rocky-and-rihanna-celebrate-fenty-beauty-fenty-skin-at-goya-news-photo-1649776150.jpg',
+  },
+  {
+    'name': 'Tion Wayne',
+    'message': 'Yo, you got any drill artiste friends',
+    'time': '7:25 am',
+    'profilePic':
+        'https://www.the-rhythm.com/sites/g/files/g2000009916/files/inline-images/Tion.jpg',
+  },
+  {
+    'name': 'Serti',
+    'message': 'I don dey eye this VGCli',
+    'time': '6:02 am',
+    'profilePic': 'https://avatars.githubusercontent.com/u/66566368?v=4',
+  },
+  {
+    'name': 'Abrhm.py',
+    'message': 'normally, him must collect.',
+    'time': '4:56 am',
+    'profilePic':
+        'https://media-exp1.licdn.com/dms/image/C4D03AQFeerLAJI5uqg/profile-displayphoto-shrink_200_200/0/1612773921913?e=1655337600&v=beta&t=i1lofo0CgVJrRaEU4s5NPtvNEBjHRFWHMl4PA_5ywfo',
+  },
+  {
+    'name': '+234 701 234 5678',
+    'message': 'Hi',
+    'time': '1:00 am',
+    'profilePic':
+        'https://media.istockphoto.com/photos/male-silhouette-as-avatar-profile-picture-picture-id519078727?b=1&k=20&m=519078727&s=170667a&w=0&h=XaMGjRxLslFfWrJeW4AvoM0oqMX-bx7_2VSK3sEqXNs=',
+  },
+];
+
+const messages = [
+  {
+    "isMe": false,
+    "text": "Hey What is up with you!!",
+    "time": "10:00 am",
+  },
+  {
+    "isMe": true,
+    "text": "im fine,wbu?",
+    "time": "11:00 am",
+  },
+  {
+    "isMe": false,
+    "text": "I am great man!",
+    "time": "11:01 am",
+  },
+  {
+    "isMe": false,
+    "text": "Twitter is mine",
+    "time": "11:01 am",
+  },
+  {
+    "isMe": true,
+    "text": "Normally!",
+    "time": "11:03 am",
+  },
+  {
+    "isMe": false,
+    "text": "Na who get money dey reign now",
+    "time": "11:04 am",
+  },
+  {
+    "isMe": true,
+    "text": "Chai, God when",
+    "time": "11:05 am",
+  },
+  {
+    "isMe": false,
+    "text": "After Twitter, I fit just reason Mark",
+    "time": "11:06 am",
+  },
+  {
+    "isMe": true,
+    "text": "Facebook in the trenches, lol",
+    "time": "11:15 am",
+  },
+  {"isMe": false, "text": "You no go like send aza", "time": "11:17 am",},
+  {"isMe": false, "text": "Im feeling good today", "time": "11:16 am",},
+  {"isMe": true, "text": "Bossman!", "time": "11:17 am",},
+  {
+    "isMe": false,
+    "text": "Oya send am quick",
+    "time": "11:18 am",
+  },
+  {
+    "isMe": true,
+    "text": "2135919282 UBA DEDEKE",
+    "time": "11:19 am",
+  },
+  {
+    "isMe": false,
+    "text": "Done!",
+    "time": "11:20 am",
+  },
+];
